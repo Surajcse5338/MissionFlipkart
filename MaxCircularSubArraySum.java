cb8cd7ee0f10b7c30f27b3119a794cde176a025c
Surajcse5338 <57870655+Surajcse5338@users.noreply.github.com>
2020-05-03 12:11:15 +0530

    Update MaxCircularSubArraySum.java

--- MaxCircularSubArraySum.java
+++ MaxCircularSubArraySum.java
@@ -2,9 +2,10 @@ public class MaxCircularSubArraySum{
 	public static void main(String[] args){
 		int[] A = {8,-4,3,-5,4};
 		int len=A.length;
-		int asnw1= method1(A,len);
-		int answ2=method2(A,len);
-		System.out.println(answ1+" "+answ2);
+		int asnw1 = method1(A,len);
+		int answ2 = method2(A,len);
+		int answ3 = method3(A,len);
+		System.out.println(answ1+" "+answ2+" "+answ3);
 	}
 	
 	private static int method1(int[] A,int len){
@@ -38,6 +39,20 @@ public class MaxCircularSubArraySum{
 			res=Math.max(max,res);
 		}
 		return res;
+	}
+	private static int methode3(int[] A, int len){
+		//int len=A.length;
+		int arraySum=0;
+		for(int i=0;i<len;i++){
+			arraySum=arraySum+A[i];
+		}
+		int minSum=A[0];
+		int tempSum=minSum;
+		for(int j=1;j<len;j++){
+			tempSum=Math.min(tempSum+A[j],A[j]);
+			minSum=Math.min(tempSum,minSum);
+		}
+		return (arraySum- minSum);
 	}
 
 
