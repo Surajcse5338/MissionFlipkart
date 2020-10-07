de9bac0b44c9fd3f77086606b20348804dce6a4c
Surajcse5338 <57870655+Surajcse5338@users.noreply.github.com>
2020-10-07 20:09:16 +0530

    Update CoinChangeRecDP.java

--- DP/Unbounded KS/Maximize The Cut Segment/CoinChangeRecDP.java
+++ DP/Unbounded KS/Maximize The Cut Segment/CoinChangeRecDP.java
@@ -1,4 +1,23 @@
-public long count(int S[], int m, int n) 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class CoinChangeRec
+ {
+	public static void main (String[] args)
+	 {
+	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
+	    while(t-->0){
+	        int m=sc.nextInt();
+          int n= sc.nextInt();
+	        int[] S=new int[m];
+	        for(int i=0;i<3;i++){
+	            S[i]=sc.nextInt();
+	        }
+	        System.out.println(count(S, m, n)); 
+	    }
+	 }
+static long count(int S[], int m, int n) 
     { 
         
         long[][] dp=new long[m+1][n+1];
@@ -21,3 +40,4 @@ public long count(int S[], int m, int n)
         }
         return dp[m][n];
     } 
+}
